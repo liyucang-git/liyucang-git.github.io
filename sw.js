3b87fb708abc2fe17fab48e7f3ecde0404bec6da
liyucang-git； <765313580@qq.com>
2019-04-06 18:49:25 +0800

    blog

--- sw.js
+++ sw.js
@@ -1,18 +1,7 @@
-/* ===========================================================
- * sw.js
- * ===========================================================
- * Copyright 2016 @huxpro
- * Licensed under Apache 2.0 
- * Register service worker.
- * ========================================================== */
-
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
-  "cdnjs.cloudflare.com"
 ]
 
 
@@ -80,8 +69,8 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(PRECACHE).then(cache => {
       return cache.add('offline.html')
-      .then(self.skipWaiting())
-      .catch(err => console.log(err))
+        .then(self.skipWaiting())
+        .catch(err => console.log(err))
     })
   )
 });
@@ -93,7 +82,7 @@ self.addEventListener('install', e => {
  *
  *  waitUntil(): activating ====> activated
  */
-self.addEventListener('activate',  event => {
+self.addEventListener('activate', event => {
   console.log('service worker activated.')
   event.waitUntil(self.clients.claim());
 });
@@ -113,9 +102,9 @@ self.addEventListener('fetch', event => {
 
   // Skip some of cross-origin requests, like those for Google Analytics.
   if (HOSTNAME_WHITELIST.indexOf(new URL(event.request.url).hostname) > -1) {
-    
+
     // Redirect in SW manually fixed github pages 404s on repo?blah 
-    if(shouldRedirect(event.request)){
+    if (shouldRedirect(event.request)) {
       event.respondWith(Response.redirect(getRedirectUrl(event.request)))
       return;
     }
@@ -125,7 +114,7 @@ self.addEventListener('fetch', event => {
     // Upgrade from Jake's to Surma's: https://gist.github.com/surma/eb441223daaedf880801ad80006389f1
     const cached = caches.match(event.request);
     const fixedUrl = getFixedUrl(event.request);
-    const fetched = fetch(fixedUrl, {cache: "no-store"});
+    const fetched = fetch(fixedUrl, { cache: "no-store" });
     const fetchedCopy = fetched.then(resp => resp.clone());
 
     // Call respondWith() with whatever we get first.
@@ -142,7 +131,7 @@ self.addEventListener('fetch', event => {
     event.waitUntil(
       Promise.all([fetchedCopy, caches.open(RUNTIME)])
         .then(([response, cache]) => response.ok && cache.put(event.request, response))
-        .catch(_ => {/* eat any errors */})
+        .catch(_ => {/* eat any errors */ })
     );
   }
 });
